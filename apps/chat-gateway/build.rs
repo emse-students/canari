fb1ad4a85446b0dfaba20c9f9fd97a0a646fe27f
Jolan BOUDIN <jolan.boudin.jourdan@gmail.com>
2026-03-09 00:56:29 +0100

    fix(gateway): allow clippy::enum_variant_names on generated MediaKind

--- apps/chat-gateway/build.rs
+++ apps/chat-gateway/build.rs
@@ -5,6 +5,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     unsafe { std::env::set_var("PROTOC", &protoc) };
 
     prost_build::Config::new()
+        // MediaKind variants are prefixed with "Media" by prost convention;
+        // allow it explicitly to satisfy clippy::enum_variant_names.
+        .type_attribute("MediaKind", "#[allow(clippy::enum_variant_names)]")
         .compile_protos(&["../../libs/proto/canari.proto"], &["../../libs/proto/"])?;
     println!("cargo:rerun-if-changed=../../libs/proto/canari.proto");
     Ok(())
