14e25a735e8b6deb0556c30a2a05291503d2a357
Jolan BOUDIN <jolan.boudin.jourdan@gmail.com>
2026-04-10 10:51:48 +0200

    chore(android): migrate google-services plugin to DSL v4.4.4, Firebase BoM 34.12.0

--- frontend/src-tauri/gen/android/build.gradle.kts
+++ frontend/src-tauri/gen/android/build.gradle.kts
@@ -1,3 +1,8 @@
+plugins {
+    // Google services Gradle plugin (apply false = activé uniquement dans les modules qui l'utilisent)
+    id("com.google.gms.google-services") version "4.4.4" apply false
+}
+
 buildscript {
     repositories {
         google()
@@ -6,7 +11,6 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.11.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.25")
-        classpath("com.google.gms:google-services:4.4.2")
     }
 }
 
