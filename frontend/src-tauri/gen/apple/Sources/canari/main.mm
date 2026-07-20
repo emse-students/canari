adba51fe279d20cb988dbbb4e45ebda0d27cc37b
Léon Muselli <leon.muselli@protonmail.com>
2026-07-20 21:24:49 +0200

    add FCM to ios

--- frontend/src-tauri/gen/apple/Sources/canari/main.mm
+++ frontend/src-tauri/gen/apple/Sources/canari/main.mm
@@ -1,6 +1,21 @@
 #include "bindings/bindings.h"
 #include "canari_ios.h"
 
+#import <UIKit/UIKit.h>
+#import <FirebaseCore/FirebaseCore.h>
+
+@interface TauriAppDelegate : UIResponder <UIApplicationDelegate>
+@end
+
+@implementation TauriAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(nullable NSDictionary<UIApplicationLaunchOptionsKey,id> *)launchOptions {
+    [FIRApp configure];
+    
+    return YES;
+}
+@end
+
 int main(int argc, char * argv[]) {
 	canari_ios_bootstrap();
 	ffi::start_app();
